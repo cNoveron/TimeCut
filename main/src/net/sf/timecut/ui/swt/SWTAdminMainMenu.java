1b36c9957ccde11cdc80e81462ba3703e41b3363
carlosescom <carlos.escom.ipn@gmail.com>
2017-06-12 21:16:53 -0500

    Revert ""
    This reverts commit 6035d755752791bee3945c59958efbb9ec1a79a7.

---
+++ main/src/net/sf/timecut/ui/swt/SWTAdminMainMenu.java
@@ -0,0 +1,393 @@
+/*
+ * Copyright (c) Rustam Vishnyakov, 2005-2010 (dyadix@gmail.com)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * $Id: SWTAdminMainMenu.java,v 1.29 2011/01/21 08:00:10 dyadix Exp $
+ */
+package net.sf.timecut.ui.swt;
+
+import java.io.File;
+
+import net.sf.timecut.ui.swt.search.FindDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+
+import net.sf.timecut.ResourceHelper;
+import net.sf.timecut.DataManager;
+import net.sf.timecut.model.*;
+import net.sf.timecut.ui.swt.help.HelpProvider;
+import net.sf.timecut.ui.swt.help.SWTAboutDialog;
+import net.sf.timecut.ui.swt.prefs.PreferencesDialog;
+import net.sf.timecut.ui.swt.tasklist.TaskListView;
+
+/**
+ * Main DataManager menu.
+ * 
+ * @author rvishnyakov
+ */
+public class SWTAdminMainMenu {
+    
+    public final static int MAX_FILE_CHARS = 40;
+    private Shell         _parent;
+    private SWTWindow _Window;
+    private Menu          _mainMenu;
+    private MenuItem      _saveItem;
+    private MenuItem      _fileMenu;
+    private MenuItem      _addMenuItem;
+    private MenuItem      _deleteItem;
+    private MenuItem      _startTimerItem;
+
+    public SWTAdminMainMenu(SWTWindow Window) {
+        _parent = Window.getShell();
+        _Window = Window;
+        setup();
+    }
+
+
+    public void setSaveItemEnabled(boolean enabled) {
+        _saveItem.setEnabled(enabled);
+    }
+
+
+    public void updateFileMenu() {
+        _fileMenu.dispose();
+        _fileMenu = createFileMenu(0);
+    }
+
+
+    public void updateOnSelection(Object selection) {
+        _addMenuItem.setEnabled(false);
+        if (selection instanceof Project) {
+            _addMenuItem.setEnabled(true);
+        }
+        if (selection instanceof IdleTask || selection instanceof Workspace) {
+            _deleteItem.setEnabled(false);
+        }
+        else {
+            _deleteItem.setEnabled(true);
+        }
+        _startTimerItem.setEnabled(selection instanceof Task);
+    }
+
+
+    private void setup() {
+        _mainMenu = new Menu(_parent, SWT.BAR);
+        _fileMenu = createFileMenu(0);
+        createEditMenu(1);
+        createSearchMenu(2);
+        createViewMenu(3);
+        createToolsMenu(4);
+        createHelpMenu(5);
+        _parent.setMenuBar(_mainMenu);
+    }
+
+
+    private MenuItem createFileMenu(int order) {
+        MenuItem fileItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        fileItem.setText(ResourceHelper.getString("menu.file"));
+        Menu fileMenu = new Menu(_parent, SWT.DROP_DOWN);
+        fileItem.setMenu(fileMenu);
+        // 
+        // New
+        //        
+        MenuItem newItem = new MenuItem(fileMenu, SWT.CASCADE);
+        newItem.setText(ResourceHelper.getString("menu.new"));
+        newItem
+            .setImage(_Window.getIconSet().getIcon("newWorkspace", true));
+        newItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().resetWorkspace();
+            }
+        });
+        //
+        // Open
+        //
+        MenuItem openItem = new MenuItem(fileMenu, SWT.CASCADE);
+        openItem.setText(ResourceHelper.getString("menu.open"));
+        openItem.setImage(_Window.getIconSet().getIcon("open", true));
+        openItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().openWorkspace();
+            }
+        });
+        //
+        // Save
+        //
+        _saveItem = new MenuItem(fileMenu, SWT.CASCADE);
+        _saveItem.setText(ResourceHelper.getString("menu.save") + "\tCtrl+S");
+        _saveItem.setEnabled(false);
+        _saveItem.setImage(_Window.getIconSet().getIcon("save", true)); 
+        _saveItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().saveWorkspace(true);
+            }
+        });
+        _saveItem.setAccelerator(SWT.CTRL | 'S');
+        // 
+        // Save As
+        //
+        MenuItem saveAsItem = new MenuItem(fileMenu, SWT.CASCADE);
+        saveAsItem.setText(ResourceHelper.getString("menu.saveAs"));
+        saveAsItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().saveWorkspace(false);
+            }
+        });
+        //      
+        // Export
+        //
+        MenuItem exportItem = new MenuItem(fileMenu, SWT.CASCADE);
+        exportItem.setText(ResourceHelper.getString("menu.export"));
+        Menu exportMenu = new Menu(exportItem);
+        exportItem.setMenu(exportMenu);
+        MenuItem exportToCsvItem = new MenuItem(exportMenu, SWT.CASCADE);
+        exportToCsvItem.setText(ResourceHelper.getString("menu.export.csv"));
+        exportToCsvItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().exportToCsv();
+            }
+        });
+        new MenuItem(fileMenu, SWT.SEPARATOR);
+
+        //
+        // Recently open files
+        //
+        createRecentlyOpenFileItems(fileMenu);
+        //
+        // Exit
+        //
+        new MenuItem(fileMenu, SWT.SEPARATOR);
+        MenuItem menuFileExit = new MenuItem(fileMenu, SWT.CASCADE);
+        menuFileExit.setText(ResourceHelper.getString("menu.exit"));
+        menuFileExit.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                DataManager.getInstance().exit();
+            }
+        });
+        return fileItem;
+    }
+
+
+    private void createEditMenu(int order) {
+        MenuItem editMenuItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        editMenuItem.setText(ResourceHelper.getString("menu.edit"));
+        Menu editMenu = new Menu(_parent, SWT.DROP_DOWN);
+        editMenuItem.setMenu(editMenu);
+        //
+        // Add menu
+        //
+        _addMenuItem = new MenuItem(editMenu, SWT.CASCADE);
+        _addMenuItem.setText(ResourceHelper.getString("menu.add"));
+        _addMenuItem.setImage(_Window.getIconSet().getIcon("add", true));
+        _Window.getMenuFactory().createAddMenu(_addMenuItem);
+        //
+        // Delete
+        //
+        _deleteItem = new MenuItem(editMenu, SWT.CASCADE);
+        _deleteItem.setText(ResourceHelper.getString("menu.delete"));
+        _deleteItem.setImage(_Window.getIconSet().getIcon("delete", true));
+        _deleteItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                DataManager.getInstance().removeSelection();
+            }
+        });
+        new MenuItem(editMenu, SWT.SEPARATOR);
+        
+        this._Window.getMenuFactory().createPropertyItem(editMenu);
+    }
+
+
+    private void createRecentlyOpenFileItems(Menu menu) {
+        File[] fileList = DataManager.getInstance().getRecentlyOpenFiles();
+        for (int i = fileList.length - 1; i >= 0; i --) {
+            MenuItem fileItem = new MenuItem(menu, SWT.CASCADE);
+            String filePath = fileList[i].getAbsolutePath();
+            if (filePath.length() > MAX_FILE_CHARS) {
+                filePath = getFileRoot(fileList[i]) + File.separator
+                    + fileList[i].getParentFile().getName() + File.separator
+                    + fileList[i].getName();
+                if (filePath.length() > MAX_FILE_CHARS) {
+                    filePath = getFileRoot(fileList[i]) + File.separator
+                        + fileList[i].getName();
+                    if (filePath.length() > MAX_FILE_CHARS) {
+                        if (MAX_FILE_CHARS > fileList[i].getName().length()) {
+                            filePath = getFileRoot(fileList[i])
+                                + File.separator + fileList[i].getName();
+                        }
+                        else {
+                            filePath = getFileRoot(fileList[i])
+                                + File.separator
+                                + fileList[i].getName().substring(
+                                    0,
+                                    MAX_FILE_CHARS) + "...";
+                        }
+                    }
+                }
+            }
+            fileItem.setText(filePath);
+            fileItem.setData(fileList[i].getAbsolutePath());
+            fileItem.addSelectionListener(new SelectionListener() {
+
+                public void widgetSelected(SelectionEvent e) {
+                    MenuItem item = (MenuItem) e.getSource();
+                    File file = new File((String) item.getData());
+                    DataManager.getInstance().loadWorkspace(file);
+                }
+
+
+                public void widgetDefaultSelected(SelectionEvent e) {
+                    // Do nothing
+                }
+            });
+        }
+    }
+    
+    /*
+     * Returns a file root as /bin/... or C:/... 
+     */
+    private String getFileRoot(File file) {
+        String path = file.getParentFile().getAbsolutePath();
+        int pos = path.indexOf(File.separatorChar, 1);
+        if (pos > 0) {
+            if (pos < file.length() - 1) {
+                path = path.substring(0, pos) + File.separator + "...";
+            }
+        }
+        else {
+            path = "...";
+        }
+        return path;
+    }
+
+
+    private void createHelpMenu(int order) {
+        MenuItem helpMenuItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        helpMenuItem.setText(ResourceHelper.getString("menu.help"));
+        Menu helpMenu = new Menu(helpMenuItem);
+        helpMenuItem.setMenu(helpMenu);
+        
+        MenuItem helpItem = new MenuItem(helpMenu, SWT.CASCADE);
+        helpItem.setText(ResourceHelper.getString("menu.helpContent"));
+        helpItem.setImage(_Window.getIconSet().getIcon("help", true));
+        helpItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                //HelpWindow helpWindow = new HelpWindow(_Window);
+                //helpWindow.open();
+                HelpProvider.openHelp();
+            }
+        });                
+        
+        //new MenuItem(helpMenu, SWT.SEPARATOR);
+        
+        MenuItem webPageItem = new MenuItem(helpMenu, SWT.CASCADE);
+        webPageItem.setText(ResourceHelper.getString("menu.homepage"));
+        webPageItem.setImage(_Window.getIconSet().getIcon("homepage", true));
+        webPageItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                Program.launch("http://timecult.sf.net");
+            }
+        });                
+        
+        new MenuItem(helpMenu, SWT.SEPARATOR); 
+        
+        MenuItem aboutItem = new MenuItem(helpMenu, SWT.CASCADE);
+        aboutItem.setText(ResourceHelper.getString("menu.about"));
+        aboutItem.setImage(_Window.getIconSet().getIcon("about", true));
+        aboutItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                SWTAboutDialog aboutDialog = SWTAboutDialog.getInstance(_parent);
+                aboutDialog.open();
+            }
+        });
+
+    }
+    
+    private MenuItem createViewMenu(int order) {
+        MenuItem viewItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        viewItem.setText(ResourceHelper.getString("menu.view"));
+        Menu viewMenu = new Menu(_parent, SWT.DROP_DOWN);
+        viewItem.setMenu(viewMenu);
+        
+        MenuItem flaggedItem = new MenuItem(viewMenu, SWT.CASCADE);
+        flaggedItem.setText(ResourceHelper.getString("tasklist.title") + "\tCtrl+T");
+        flaggedItem.setImage(_Window.getIconSet().getIcon("tasklist", true));
+        flaggedItem.setAccelerator(SWT.CTRL | 'T');
+        flaggedItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                TaskListView taskListView = new TaskListView(_Window);
+                taskListView.open();
+            }
+        });
+        
+        return viewItem;
+    }
+    
+    private MenuItem createToolsMenu(int order) {
+        MenuItem toolsItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        toolsItem.setText(ResourceHelper.getString("menu.tools"));
+        Menu toolsMenu = new Menu(_parent, SWT.DROP_DOWN);
+        toolsItem.setMenu(toolsMenu);
+
+        _startTimerItem = new MenuItem(toolsMenu, SWT.CASCADE);
+        _startTimerItem.setText(ResourceHelper.getString("button.start.tooltip") + "\tCtrl+R");
+        _startTimerItem.setImage(_Window.getIconSet().getIcon("start", true));
+        _startTimerItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                DataManager.getInstance().startTimer();
+            }
+        });
+        _startTimerItem.setAccelerator(SWT.CTRL + 'R');
+        
+        MenuItem optionsItem = new MenuItem(toolsMenu, SWT.CASCADE);
+        optionsItem.setText(ResourceHelper.getString("menu.options"));
+        optionsItem.setImage(_Window.getIconSet().getIcon("options", true));
+        optionsItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                PreferencesDialog prefsDialog =  PreferencesDialog.getInstance(_parent) ;
+                prefsDialog.open();
+            }
+        });
+        
+        return toolsItem;
+    }
+
+    private MenuItem createSearchMenu(int order) {
+        MenuItem searchItem = new MenuItem(_mainMenu, SWT.CASCADE, order);
+        searchItem.setText(ResourceHelper.getString("menu.search"));
+        Menu searchMenu = new Menu(_parent, SWT.DROP_DOWN);
+        searchItem.setMenu(searchMenu);
+
+        MenuItem findItem = new MenuItem(searchMenu, SWT.CASCADE);
+        findItem.setText(ResourceHelper.getString("menu.search.find") + "\tCtrl+F");
+        findItem.setImage(_Window.getIconSet().getIcon("find", true));
+        findItem.setAccelerator(SWT.CTRL | 'F');
+        findItem.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent evt) {
+                FindDialog findDialog = new FindDialog(_Window, DataManager.getInstance().getWorkspace());
+                findDialog.open();
+            }
+        });
+
+        return searchItem;
+    }
+
+}
---
+++ main/src/net/sf/timecut/ui/swt/filter/AdminAdvancedTimeFilterView.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) Rustam Vishnyakov, 2005-2009 (dyadix@gmail.com)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * $Id: AdminAdvancedTimeFilterView.java,v 1.5 2010/09/26 12:13:31 dyadix Exp $
+ */
+package net.sf.timecut.ui.swt.filter;
+
+import java.util.HashMap;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.List;
+
+import net.sf.timecut.ResourceHelper;
+import net.sf.timecut.DataManager;
+import net.sf.timecut.model.CustomFilterList;
+import net.sf.timecut.model.TimeRecordFilter;
+import net.sf.timecut.model.TimeRecordFilterFactory;
+import net.sf.timecut.model.Workspace;
+import net.sf.timecut.ui.swt.SWTWindow;
+
+/**
+ * Replaces SWTTimeFilterView. Allows to add custom filters.
+ */
+public class AdminAdvancedTimeFilterView implements TimeFilterView {
+    
+    private DataManager              _dataManager;
+    private List                     filterList;
+    private SWTWindow            Window;
+    private HashMap<Integer, String> indexToLabel = new HashMap<Integer, String>();
+    private TimeFilterToolbar        toolbar;
+    
+    public AdminAdvancedTimeFilterView(SWTWindow Window) {
+        this._dataManager = DataManager.getInstance();
+        this.Window = Window;
+        setup();
+    }
+
+    private void setup() {
+        GridLayout contentLayout = new GridLayout();
+        contentLayout.numColumns = 1;
+        contentLayout.makeColumnsEqualWidth = true;
+        Composite contentPanel = new Composite(this.Window.getFilterContainer(), SWT.BORDER);
+        toolbar = new TimeFilterToolbar(this, contentPanel);
+        GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+        contentPanel.setLayout(contentLayout);
+        contentPanel.setLayoutData(gridData);
+        
+        //toolbar = new TimeFilterToolbar(this, contentPanel);
+        
+        createFilterList(contentPanel);
+    }
+
+    private List createFilterList(Composite contentPanel) {        
+        GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+        this.filterList = new List(contentPanel, SWT.BORDER | SWT.V_SCROLL);
+        this.filterList.setLayoutData(gridData);
+        updateFilterList();
+        this.filterList.addSelectionListener(new SelectionAdapter() {
+
+            public void widgetSelected(SelectionEvent evt) {
+                _dataManager.getWorkspace().setFilter(getSelectedFilter());                
+            }
+        });
+        return this.filterList;
+    }
+    
+    
+    /**
+     * Redraw the list of filters using the current workspace.
+     */
+    public void updateFilterList() {
+        this.filterList.removeAll();
+        //
+        // Add built-in filters
+        //
+        String[] filterLabels = TimeRecordFilterFactory.getFilterLabels();
+        TimeRecordFilter defaultFilter = this._dataManager.getWorkspace().getFilter();
+        for (int i = 0; i < filterLabels.length; i ++) {
+            String filterName = ResourceHelper.getString("filter." + filterLabels[i]);
+            this.indexToLabel.put(new Integer(i), filterLabels[i]);            
+            this.filterList.add(filterName);
+            if (defaultFilter != null && defaultFilter.getLabel().equals(filterLabels[i])) {
+                this.filterList.select(i);
+            }
+        }
+        //
+        // Add custom filters (stored in the workspace)
+        //
+        Workspace ws = this._dataManager.getWorkspace();
+        for (TimeRecordFilter customFilter : ws.getCustomFilters().asArray()) {
+            String filterName = customFilter.getLabel();            
+            this.filterList.add(filterName);
+            int index = this.filterList.indexOf(filterName);
+            this.indexToLabel.put(new Integer(index), filterName);
+            if (defaultFilter != null && defaultFilter.getLabel().equals(filterName)) {
+                this.filterList.select(index);
+            }
+        }
+        
+    }
+    
+    
+    public void setFilterSelection(TimeRecordFilter filter) {
+        boolean selected = false;
+        if (filter != null) {
+            String lcName = "";
+            if (!filter.isCustom()) {
+                lcName = ResourceHelper.getString("filter."
+                    + filter.getLabel());
+                toolbar.setDeleteEnabled(false);
+            }
+            else {
+                lcName = filter.getLabel();
+                toolbar.setDeleteEnabled(true);
+            }
+            String items[] = this.filterList.getItems();
+            for (int i = 0; i < this.filterList.getItemCount() && !selected; i++) {
+                if (items[i].equals(lcName)) {
+                    this.filterList.setSelection(i);
+                    this.filterList.showSelection();
+                    selected = true;
+                }
+            }
+        }
+        if (!selected) {
+            this.filterList.deselectAll();
+        }
+    }
+    
+    public SWTWindow getWindow() {
+        return this.Window;
+    }
+
+    /**
+     * Add custom filter to the workspace and set it as a current filter.
+     * @param filter The filter to add.
+     */
+    public void addCustomFilter(TimeRecordFilter filter) {        
+        this.filterList.add(filter.getLabel());
+        int index = this.filterList.indexOf(filter.getLabel());
+        this.indexToLabel.put(new Integer(index), filter.getLabel());
+        Workspace ws = DataManager.getInstance().getWorkspace();
+        CustomFilterList customFilters = ws.getCustomFilters();
+        customFilters.addFilter(filter);
+        ws.setFilter(filter);
+        setFilterSelection(filter);
+        toolbar.setDeleteEnabled(true);
+    }
+    
+    public void deleteSelectedFilter() {
+        TimeRecordFilter filter = getSelectedFilter();
+        if (filter != null && filter.isCustom()) {
+            _dataManager.getWorkspace().getCustomFilters().removeFilter(filter);
+            toolbar.setDeleteEnabled(false);
+            updateFilterList();
+        }
+    }
+    
+    private TimeRecordFilter getSelectedFilter() {
+        Integer index = new Integer(AdminAdvancedTimeFilterView.this.filterList.getSelectionIndex());
+        String label = AdminAdvancedTimeFilterView.this.indexToLabel.get(index);
+        if (label != null) {
+            toolbar.setDeleteEnabled(false);
+            TimeRecordFilter filter = TimeRecordFilterFactory.createFilter(label);
+            if (filter == null) {
+                CustomFilterList customFilters = DataManager
+                    .getInstance().getWorkspace().getCustomFilters();
+                filter = customFilters.getFilter(label);
+                if (filter != null) {
+                    toolbar.setDeleteEnabled(true);
+                }
+            }
+            return filter;
+        }
+        return null;
+    }
+
+}
